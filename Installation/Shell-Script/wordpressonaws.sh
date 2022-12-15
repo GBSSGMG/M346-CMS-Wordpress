68f5bf031237191a22bc75ce6d3de9714998c93a
Giosue Masiello <Giosue.Masiello@edu.gbssg.ch>
2022-12-15 11:00:34 +0100

    Syntaxfehler korrigiert
    Pfad war nicht sauber

--- Installation/Shell-Script/wordpressonaws.sh
+++ Installation/Shell-Script/wordpressonaws.sh
@@ -11,4 +11,4 @@ aws ec2 authorize-security-group-ingress --group-name wp-sec-group --protocol tc
 
 aws ec2 authorize-security-group-ingress --group-name wp-sec-group --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name autowordpress --security-groups wp-sec-group --iam-instance-profil Name=LabInstanceProfile --user-data file://~/M346-CMS-WORDPRESS/Installation/Cloud-Init/cloud-config.yaml --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Wordpress}]'
+aws ec2 run-instances --image-id ami-08c40ec9ead489470 --count 1 --instance-type t2.micro --key-name autowordpress --security-groups wp-sec-group --iam-instance-profil Name=LabInstanceProfile --user-data file://M346-CMS-WORDPRESS/Installation/Cloud-Init/cloud-config.yaml --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Wordpress}]'
